beec0e49d4671aec585e7952636e8110921cc5d3
PMS.UI <kza91@hotmail.com>
2023-09-18 18:25:56 +0200

    startUp

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input({
+    required: true
+  })
+  course:Course;
+  
+  constructor() {
+     
+  }
+  ngOnInit(): void {
+    
+  }
+
+}
